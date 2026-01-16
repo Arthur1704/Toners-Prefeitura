33ae05a6d86e035fc01a5b0da102f07e09cbfa0e
Arthur <arthurcruzviana19@gmail.com>
2026-01-16 15:30:43 -0300

    Projeto Finalizado

--- tonerspref/tonerspref/src/main/resources/static/Js/printer.js
+++ tonerspref/tonerspref/src/main/resources/static/Js/printer.js
@@ -4,6 +4,26 @@ $(document).ready(function(){
 
         const id = $(this).data('id');
         const modelo = $(this).data('modelo')
-    })
 
-})
+        $('#editarId').val(id);
+        $('#editar').val(modelo)
+
+        $('#modalEditar').modal('show');
+    });
+
+
+    $('.btn-deletar').on('click', function(){
+
+        //Pega os dados do botão clicado
+        const id = $(this).data('id');
+        const modelo = $(this).data('modelo');
+
+        // Preenche o modal
+        $('#deletarId').val(id);
+        $('#deletarModelo').text(modelo);
+
+        //Abre o modal
+        $('#modalDeletar').modal('show');
+    });
+
+});
